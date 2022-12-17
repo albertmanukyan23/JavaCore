964151df9e858e39d0d291d616ed3c7c16561952
Albert Manukyan <albertmanukyan975@gmail.com>
2022-12-17 19:26:45 +0400

    homework

---
+++ src/homework/homework1/FigurePainter.java
@@ -0,0 +1,38 @@
+package homework.homework1;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+        System.out.println("Figure 1");
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+
+        System.out.println("Figure 2");
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; i < j; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Figure 3");
+        for (int i = 4; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
