047649142c05f07e095af10a54b735dcbaf3ffee
Albert Manukyan <albertmanukyan975@gmail.com>
2022-12-17 19:53:38 +0400

    homework

--- src/homework/homework1/FigurePainter.java
+++ src/homework/homework1/FigurePainter.java
@@ -19,7 +19,7 @@ public class FigurePainter {
                 System.out.print("  ");
             }
             for (int j = 0; j <= i; j++) {
-                System.out.print(" *");
+                System.out.print("* ");
             }
             System.out.println();
         }
