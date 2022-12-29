34558a8adcefe6f39308b32fe3f773dec902cc96
Albert Manukyan <albertmanukyan975@gmail.com>
2022-12-29 23:04:30 +0400

    homework4

--- src/homework/homework4/CharArrayExample.java
+++ src/homework/homework4/CharArrayExample.java
@@ -7,13 +7,13 @@ public class CharArrayExample {
         //Պետք է գրել լոգիկա, որը կհաշվի թե քանի հատ կա c-փոփոխականից մեր մասիվի մեջ, ու տպել այդ քանակը։
         char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e' };
         char c = 'o';
-        int value = 0;
+        int oCount = 0;
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == c) {
-                value++;
+                oCount++;
             }
         }
-        System.out.println("c-փոփոխականից կա " + value + " հատ");
+        System.out.println("c-փոփոխականից կա " + oCount + " հատ");
 
         //գրել կոդ, որը կտպի մեջտեղի 2 սինվոլները
         int midIndex1 = chars.length / 2;
