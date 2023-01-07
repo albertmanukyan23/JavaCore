2e1d6d1564c4812fb4ade1fa702d5033f77fecf4
Albert Manukyan <albertmanukyan975@gmail.com>
2023-01-07 18:46:15 +0400

    homework 5

--- src/homework/homework5/ArraySpaceExample.java
+++ src/homework/homework5/ArraySpaceExample.java
@@ -7,20 +7,18 @@ public class ArraySpaceExample {
         int startCount = 0;
         int endCount = 0;
         //գտնում ենք թե քանի բացատ կա մասիվի սկզբից մինչև առարջին սինվոլը
-        for (char c : spaceArray) {
-            if (c != ' ') {
+        for (int i = 0; i < spaceArray.length; i++) {
+            if (spaceArray[i] != ' ') {
                 break;
             }
             startCount++;
-
         }
         // գտնում ենք թե քանի բացատ կա վերջից մինչև առաջին հանդիպող սինվոլը
-        for (char c : spaceArray) {
-            if (c != ' ') {
+        for (int j = spaceArray.length - 1; j > 0; j--) {
+            if (spaceArray[j] != ' ') {
                 break;
             }
             endCount++;
-
         }
         char[] result = new char[spaceArray.length - (startCount + endCount)];
         System.out.print("result: ");
