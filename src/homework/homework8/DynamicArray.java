3a0deee317fd16f4db6930caae1277a4902fa089
Albert Manukyan <albertmanukyan975@gmail.com>
2023-01-18 12:52:43 +0400

    homework7

---
+++ src/homework/homework8/DynamicArray.java
@@ -0,0 +1,83 @@
+package homework.homework8;
+
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
+public class DynamicArray {
+
+    //սա մեր հիմնական մասիվն է, որտեղ պահելու ենք ավելացվող էլեմենտները
+    private int[] array = new int[10];
+    //սա մեր մասիվի մեջ ավելացված էլեմենտների քանակն է
+    private int size = 0;
+
+    //ստուգել եթե մասիվի մեջ տեղ չկա-> կանչել extend()
+    //և ավելացնենք
+    public void add(int value) {
+        if (size == array.length) {
+            extend();
+        }
+        array[size++] = value;
+    }
+
+    //1․ ստեղծել հին մասիվից 10 էլեմենտ ավելի մեծ մասիվ
+    //2․ քցել հին մասիվի էլեմենտները նորի մեջ
+    //3․ հին մասիվի հղումը կապենք նոր մասիվի հղման հետ։
+    private void extend() {
+        int[] newArray = new int[array.length + 10];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+
+    //եթե տրված ինդեքսը մեր ունեցած մասիվի ինդեքսի սահմաններում է, վերադարձնել
+    // մասիվի index-երրորդ էլեմենտը։ Հակառակ դեպքում վերադարձնել -1։
+    public int getByIndex(int index) {
+        if (index >= 0 && index < size) {
+            return array[index];
+        }
+        return -1;
+    }
+
+    //տպել մասիվի ավելացված էլեմենտները
+    public void print() {
+        if (size == 0) {
+            System.err.println("Your Array is empty! ");
+            return;
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    //ջնջել մասիվի էլեմենտը ինդեքսով
+    public void deleteByIndex(int index) {
+        if (index < 0 || index >= size) {
+            System.err.println("Wrong index! ");
+            return;
+        }
+        int[] newArray = new int[--size];
+        for (int i = 0; i < newArray.length; i++) {
+            if (i >= index) {
+                newArray[i] = array[i + 1];
+                continue;
+            }
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+
+    // ջնջել միջակայքի հատվածով
+    public void deleteByIndex(int leftBoard, int rightBoard) {
+        if (leftBoard < rightBoard) {
+            int changedSize = rightBoard - leftBoard;
+            int[] newArray = new int[changedSize];
+            for (int i = 0; i < newArray.length; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+            size = changedSize;
+        } else {
+            System.err.println("Indexes are incorrect! ");
+        }
+    }
+}
---
+++ src/homework/homework8/DynamicArrayDemo.java
@@ -0,0 +1,27 @@
+package homework.homework8;
+
+public class DynamicArrayDemo {
+    public static void main(String[] args) {
+        DynamicArray da = new DynamicArray();
+        // test of add
+        for (int i = 0; i < 15; i++) {
+            da.add(4);
+        }
+        da.print();
+        // test getByIndex
+        System.out.println();
+        da.add(5);
+        System.out.println(da.getByIndex(15));
+        // test deleteByIndex
+        da.add(18);
+        da.add(19);
+        da.deleteByIndex(16);
+        da.print();
+        // test the second  deleteByIndex function
+        System.out.println();
+        da.deleteByIndex(14, 16);
+        da.print();
+        da.deleteByIndex(5);
+
+    }
+}
