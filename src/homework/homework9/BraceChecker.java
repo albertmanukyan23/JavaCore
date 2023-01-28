374b457c70da0a3ce584b676ef3edcc079a4597c
Albert Manukyan <albertmanukyan975@gmail.com>
2023-01-28 23:09:26 +0400

    homework9 continue

--- src/homework/homework9/BraceChecker.java
+++ src/homework/homework9/BraceChecker.java
@@ -54,4 +54,82 @@ public class BraceChecker {
             System.err.println("The closing brace  at the end is missing");
         }
     }
+
+    public String replaceWrongBraces() {
+        Stack stack = new Stack();
+        StringBuilder sb = new StringBuilder(text);
+        // Stack դատարկ լինելու դեպքում pop()-ը վերադարձնում է '+';
+        for (int i = 0; i < sb.length(); i++) {
+            switch (sb.charAt(i)) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(sb.charAt(i));
+                    break;
+                case ')':
+                    char firstType = stack.pop();
+                    if (firstType == '+') {
+                        sb.deleteCharAt(i);
+                        i--;
+                        break;
+                    }
+                    if (firstType == '[') {
+                        sb.setCharAt(i, ']');
+                        break;
+                    }
+                    if (firstType == '{') {
+                        sb.setCharAt(i, '}');
+                        ;
+                    }
+                    break;
+                case ']':
+                    char secondType = stack.pop();
+                    if (secondType == '+') {
+                        sb.deleteCharAt(i);
+                        i--;
+                        break;
+                    }
+                    if (secondType == '{') {
+                        sb.setCharAt(i, '}');
+                        break;
+                    }
+                    if (secondType == '(') {
+                        sb.setCharAt(i, ')');
+                    }
+                    break;
+                case '}':
+                    char thirdType = stack.pop();
+                    if (thirdType == '+') {
+                        sb.deleteCharAt(i);
+                        i--;
+                        break;
+                    }
+                    if (thirdType == '[') {
+                        sb.setCharAt(i, ']');
+                        break;
+                    }
+                    if (thirdType == '(') {
+                        sb.setCharAt(i, ')');
+                    }
+                    break;
+            }
+        }
+        boolean stackIsEmpty = false;
+        while (!stackIsEmpty) {
+            switch (stack.pop()) {
+                case '+':
+                    stackIsEmpty = true;
+                    break;
+                case '(':
+                    sb.append(')');
+                    break;
+                case '[':
+                    sb.append(']');
+                    break;
+                case '{':
+                    sb.append('}');
+            }
+        }
+        return sb.toString();
+    }
 }
--- src/homework/homework9/BraceCheckerTest.java
+++ src/homework/homework9/BraceCheckerTest.java
@@ -8,8 +8,14 @@ public class BraceCheckerTest {
         braceChecker2.check();
         BraceChecker braceChecker3 = new BraceChecker("(Java (is a) go[od programming] language ");
         braceChecker3.check();
-        BraceChecker braceChecker4= new BraceChecker("([[[JAVA KOTL(I)N C++}}+] ");
+        BraceChecker braceChecker4 = new BraceChecker("([[[JAVA KOTL(I)N C++}}+] ");
         braceChecker4.check();
+        BraceChecker braceChecker5 = new BraceChecker("+}}{]this is [a( te(]xt}");
+        System.out.println(braceChecker1.replaceWrongBraces());
+        System.out.println(braceChecker2.replaceWrongBraces());
+        System.out.println(braceChecker3.replaceWrongBraces());
+        System.out.println(braceChecker4.replaceWrongBraces());
+        System.out.println(braceChecker5.replaceWrongBraces());
 
     }
 }
