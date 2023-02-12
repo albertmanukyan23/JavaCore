4b09c25fdc5ab7721da905eb189587722f188b69
Albert Manukyan <albertmanukyan975@gmail.com>
2023-02-12 12:52:38 +0400

    arrayutilDemo

--- src/homework/homework6/ArrayUtilDemo.java
+++ src/homework/homework6/ArrayUtilDemo.java
@@ -1,7 +1,5 @@
 package homework.homework6;
 
-import java.util.Arrays;
-
 public class ArrayUtilDemo {
     public static void main(String[] args) {
         int[] array = {2, 5, 8, 10, 88, 22, 11, 66, 12, 30, 6, 4, 3};
